1796ff25651951da48c155a03e344e90c19289ed
LT-321 <1256537107@qq.com>
2020-02-20 07:39:09 +0800

    frist commit

---
+++ src/main/java/com/example/pavilion/PavilionApplication.java
@@ -0,0 +1,13 @@
+package com.example.pavilion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PavilionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PavilionApplication.class, args);
+    }
+
+}
